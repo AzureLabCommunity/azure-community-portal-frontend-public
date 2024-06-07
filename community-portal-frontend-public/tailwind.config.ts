2d8afee73cb031c15fd0b8dd71a68e4f69edc87a
flowermaiden-ctb <dvernytska.ctb@outlook.com>
2024-06-07 08:59:24 +0300

    Revert "Created an event component without fetching data."
    This reverts commit 723a19bcdb98ff2dc74b255d530abd7c95cb02c3.

--- community-portal-frontend-public/tailwind.config.ts
+++ community-portal-frontend-public/tailwind.config.ts
@@ -13,22 +13,6 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      width: {
-        '360': '360px',
-      },
-      height:{
-        '400':'400px',
-      },
-      fontSize: {
-        '24':'24px',
-        '16':'16px'
-      },
-      boxShadow: {
-        'event': '0px 6px 5px -1px rgba(0,0,0,0.59)',
-      },
-      colors: {
-        'link': '#305F81',
-      },
     },
   },
   plugins: [],
